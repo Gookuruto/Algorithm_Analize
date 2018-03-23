cf213e194f03651217142ca3b0c15a8036c7a24d
Gookuruto <gokuruto1234@gmail.com>
2018-03-23 21:05:27 +0100

    nessesary changes in kMin

--- ELECTION_P.py
+++ ELECTION_P.py
@@ -25,11 +25,11 @@ def Election_p(p, n=0):
     slot = 0
     number_of_slots = 0
     round = 1
-    k=0
+    k = 0
 
-   #if(len(p)==10):
+    # if(len(p)==10):
     #   for k in p:
-   #        print(k)
+    #        print(k)
 
     if (n == 0):
         n = len(p)
@@ -41,20 +41,22 @@ def Election_p(p, n=0):
                 k = 0
                 round += 1
             slot = slot + random_choice(1, 0, p[k])
-            #print("prawdopodobienstwo= "+str(p[k]))
-            #k += 1
-            #if i==10:
-             #   break
+            # print("prawdopodobienstwo= "+str(p[k]))
+            # k += 1
+            # if i==10:
+            #   break
         k += 1
 
     return number_of_slots, round
+
+
 def probability_round_one(data_round):
-     sum=0
-     for i in data_round:
-         if i==1:
-             sum+=1
-     res=sum/len(data_round)
-     return res
+    sum = 0
+    for i in data_round:
+        if i == 1:
+            sum += 1
+    res = sum / len(data_round)
+    return res
 
 
 j = 0
@@ -65,7 +67,7 @@ while (j < 1000):
     j = j + 1
 
 for i in range(int(math.ceil(np.log2(1000)))):
-        p2.append(1 / (2 ** (i+1)))
+    p2.append(1 / (2 ** (i + 1)))
 
 j = 0
 # listy dla danych o numerze slotu
@@ -73,9 +75,9 @@ data = []
 data2 = []
 data3 = []
 data4 = []
-#dane dla scenariusza 1
+# dane dla scenariusza 1
 while (j < 1000):
-    data.append(Election_p(p,1000)[0])
+    data.append(Election_p(p, 1000)[0])
     j += 1
 # listy dla danych w której rundzie znaleziono lidera
 data_round = []
@@ -84,7 +86,7 @@ data_round3 = []
 data_round4 = []
 # Uzupelnianie list z danymi
 j = 0
-#dane dla scenariusza n=2
+# dane dla scenariusza n=2
 while (j < 1000):
     data2.append(Election_p(p2, 2)[0])
     if (data2[j] % len(p2) > 0):
@@ -93,7 +95,7 @@ while (j < 1000):
         data_round.append(data2[j] / len(p2))
     j += 1
 j = 0
-#n=u/2
+# n=u/2
 while (j < 1000):
     data3.append(Election_p(p2, 500)[0])
     if ((data3[j] % len(p2)) > 0):
@@ -102,7 +104,7 @@ while (j < 1000):
         data_round3.append(data3[j] / len(p2))
     j += 1
 j = 0
-#n=u
+# n=u
 while (j < 1000):
     data4.append(Election_p(p2, 1000)[0])
     if (data4[j] % len(p2) > 0):
@@ -123,12 +125,11 @@ print("średnia dla scenartiusza 2 przy n=2 " + str(avg_s2_n2) + " wariancja " +
 print("średnia dla scenartiusza 2 przy n=u/2  " + str(avg_s2_n_div2) + " wariancja " + str(var_s2_n_div2))
 print("średnia dla scenartiusza 2 przy n=u  " + str(avg_s2_n) + " wariancja " + str(var_s2_n))
 
-print("prob for 2 = "+str(probability_round_one(data_round)))
-print("prob for u/2= "+str(probability_round_one(data_round3)))
-print("prob for u= "+str(probability_round_one(data_round4)))
+print("prob for 2 = " + str(probability_round_one(data_round)))
+print("prob for u/2= " + str(probability_round_one(data_round3)))
+print("prob for u= " + str(probability_round_one(data_round4)))
 
 # wytworzenie histogramów w osobnych figure
-
 
 
 data.sort()
@@ -139,93 +140,88 @@ print(data)
 print(data2)
 print(data3)
 print(data4)
-round=[]
+round = []
 round.append(probability_round_one(data_round))
 round.append(probability_round_one(data_round3))
 round.append(probability_round_one(data_round4))
-data_y=[]
-data_x=[]
-how_much=0
-first=1
+data_y = []
+data_x = []
+how_much = 0
+first = 1
 for i in data:
-    if i !=first:
+    if i != first:
         data_x.append(first)
-        first=i;
+        first = i;
         data_y.append(how_much)
-        how_much=1
+        how_much = 1
     else:
-        how_much+=1
+        how_much += 1
 
-data_x2=[]
-data_y2=[]
-first=data2[0]
+data_x2 = []
+data_y2 = []
+first = data2[0]
 for i in data2:
-    if i !=first:
-        first=i;
-        data_x2.append(i-1)
-        if how_much>0:
+    if i != first:
+        first = i;
+        data_x2.append(i - 1)
+        if how_much > 0:
             data_y2.append(how_much)
-        how_much=1
+        how_much = 1
     else:
-        how_much+=1
+        how_much += 1
 
-
-
-data_x3=[]
-data_y3=[]
-first=data3[0]
+data_x3 = []
+data_y3 = []
+first = data3[0]
 for i in data3:
-    if i !=first:
-        first=i;
-        data_x3.append(i-1)
-        if how_much>0:
+    if i != first:
+        first = i;
+        data_x3.append(i - 1)
+        if how_much > 0:
             data_y3.append(how_much)
-        how_much=1
+        how_much = 1
     else:
-        how_much+=1
+        how_much += 1
 
-
-
-data_x4=[]
-data_y4=[]
-first=data4[0]
+data_x4 = []
+data_y4 = []
+first = data4[0]
 for i in data4:
-    if i !=first:
+    if i != first:
         data_x4.append(first)
-        first=i
+        first = i
         data_y4.append(how_much)
-        how_much=1
+        how_much = 1
     else:
-        how_much+=1
+        how_much += 1
 print(data_x4)
 print(data_y4)
 
-
-#sbr.figure(1)
-#sbr.barplot(data_x,data_y)
-plt.bar(data_x,data_y)
-plt.xticks(range(min(data_x), max(data_x)+1, 1))
-#ax=plt.gca()
-#plt.hist(data,align="mid",rwidth=0.5)
+# sbr.figure(1)
+# sbr.barplot(data_x,data_y)
+plt.bar(data_x, data_y)
+plt.xticks(range(min(data_x), max(data_x) + 1, 1))
+# ax=plt.gca()
+# plt.hist(data,align="mid",rwidth=0.5)
 plt.title("scenario 1")
 
 plt.figure(2)
-plt.bar(data_x2,data_y2)
-plt.xticks(range(min(data_x2), max(data_x2)+1, 1))
-#plt.hist(data2,align="mid",rwidth=0.5)
+plt.bar(data_x2, data_y2)
+plt.xticks(range(min(data_x2), max(data_x2) + 1, 1))
+# plt.hist(data2,align="mid",rwidth=0.5)
 plt.title("scenario 2 n=2")
 plt.figure(3)
-plt.bar(data_x3,data_y3)
-plt.xticks(range(min(data_x3), max(data_x3)+1, 1))
-#plt.hist(data3,align="mid",rwidth=0.5)
+plt.bar(data_x3, data_y3)
+plt.xticks(range(min(data_x3), max(data_x3) + 1, 1))
+# plt.hist(data3,align="mid",rwidth=0.5)
 plt.title("scenario 2 n=u/2")
 plt.figure(4)
-plt.bar(data_x4,data_y4)
-plt.xticks(range(min(data_x4), max(data_x4)+1, 1))
-#plt.hist(data4,align="left",rwidth=0.5)
+plt.bar(data_x4, data_y4)
+plt.xticks(range(min(data_x4), max(data_x4) + 1, 1))
+# plt.hist(data4,align="left",rwidth=0.5)
 plt.title("scenario 2 n=u")
 plt.figure(5)
 plt.plot(round)
 
-#plt.show()
+# plt.show()
 plt.show()
--- kMin.py
+++ kMin.py
@@ -1,33 +1,38 @@
 import zlib
 import hashlib
+import matplotlib.pyplot as plt
+import multiprocessing as mp
+import itertools as it
 
 def zero_one_float(a):
     y = "0." + str(a)
     y = float(y)
     return y
 
-def generate_M(n,shift):
-    x=[None]*n
+
+def generate_M(n, shift):
+    x = [None] * n
     for i in range(n):
-        x[i]=i+shift
+        x[i] = i + shift
     return x
 
 
-def kMin(k,h,M):
-    Arr=[1]*k
-    temp=1
-    counter=0
+def kMin(k, h, M):
+    Arr = [1] * k
+    temp = 1
+    counter = 0
     for i in range(len(M)):
-        temp=zero_one_float(abs(h(bytes(M[i]))))
-        if Arr[-1]==1 and temp<Arr[-1]:
-            Arr[-1]=temp
+        temp = zero_one_float(abs(int(h(M[i].to_bytes(100, 'little')).hexdigest(), 16)))
+        if temp < Arr[-1] and not (temp in Arr):
+            Arr[-1] = temp
             Arr.sort()
-            counter+=1
-    if Arr[-1]==1:
+            counter += 1
+    if Arr[-1] == 1:
         return counter
     else:
-        print(Arr[-1])
-        return (k-1)/Arr[-1]
+        # print(Arr[-1])
+        return (k - 1) / Arr[-1]
+
 
 def generate_many_M(how_many):
     x = []
@@ -38,11 +43,61 @@ def generate_many_M(how_many):
             x.append(generate_M(i + 1, x[-1][-1] + 1))
     return x
 
-def main():
-    x=generate_many_M(10000)
-    n=kMin(10,zlib.crc32,x[12])
 
-    print(n)
+def main():
+    #pool=mp.Pool(mp.cpu_count())
+    x = generate_many_M(1000)
+    nn = [None] * len(x)
+    xx = [None] * len(x)
+    nn2 = [None] * len(x)
+    nn3 = [None] * len(x)
+    nn4 = [None] * len(x)
+    nn5 = [None] * len(x)
+    nn6 = [0.0] * len(x)
+    #nn=pool.map(kMin,(2,hashlib.sha1,x))
+
+    for j in range(len(x)):
+        xx[j] = (len(x[j]))
+        nn[j] = (kMin(2, hashlib.sha1, x[j]) / len(x[j]))
+
+    for j in range(len(x)):
+        nn2[j] = (kMin(3, hashlib.sha1, x[j]) / len(x[j]))
+    for j in range(len(x)):
+        nn3[j] = (kMin(10, hashlib.sha1, x[j]) / len(x[j]))
+    for j in range(len(x)):
+        nn4[j] = (kMin(100, hashlib.sha1, x[j]) / len(x[j]))
+
+    for j in range(len(x)):
+        nn5[j] = (kMin(400, hashlib.sha1, x[j]) / len(x[j]))
+    succes = 0
+
+    for u in range(1, 1000,10):
+        for z in range(len(x)):
+            #print(z)
+            nn6[z] = (kMin(u, hashlib.sha256, x[z]) / len(x[z]))
+            if nn6[z] >= 0.9 and nn6[z] <= 1.1:
+                succes += 1
+        if succes / len(nn6) >= 0.95:
+            print("k dla ktorego 95% przypadkow zawiera sie w +- 10%" + str(u))  # k==1000
+            break
+        else:
+            succes = 0
+
+
+
+    plt.figure(1)
+    plt.plot(xx, nn, 'ro')
+    plt.figure(2)
+    plt.plot(xx, nn2, 'ro')
+    plt.figure(3)
+    plt.plot(xx, nn3, 'ro')
+    plt.figure(4)
+    plt.plot(xx, nn4, 'ro')
+    plt.figure(5)
+    plt.plot(xx, nn5, 'ro')
+    plt.show()
+
+    print("Zakonczono")
 
 
 main()
